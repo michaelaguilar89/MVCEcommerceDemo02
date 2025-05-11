2620dc32c2f82aa0afec8496d8a361af36581cd1
unknown <michaelaguilar89>
2025-05-11 00:55:30 -0600

    first commit

---
+++ Models/PaginationViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCEcommerceo1.Models
+{
+    public class PaginationViewModel
+    {
+        public List<Product> Items { get; set; } // Cambia el tipo de lista según tus items
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public string? SearchValue { get; set; }
+    }
+}
